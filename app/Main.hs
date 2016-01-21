8842e3d2e8b8985fc664fd4dae4fa299fc5d419a
John Ky <newhoggy@gmail.com>
2016-01-21 23:17:04 +1100

    Rank function

--- app/Main.hs
+++ app/Main.hs
@@ -11,5 +11,11 @@ encode :: Binary -> [Bool]
 encode (Bin l r) = True : (encode l) ++ (encode r)
 encode Tip = [False]
 
+rank :: [Bool] -> Int -> Int
+rank _ 0 = 0
+rank (True:xs) i = 1 + (rank xs (i - 1))
+rank (False:xs) i = (rank xs (i - 1))
+rank [] _ = 0
+
 main :: IO ()
 main = return ()
