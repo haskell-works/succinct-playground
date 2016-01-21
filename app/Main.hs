55210ae9a9051e3fb840cf03821b1605384748cb
John Ky <newhoggy@gmail.com>
2016-01-21 23:11:56 +1100

    Encode function

--- app/Main.hs
+++ app/Main.hs
@@ -7,5 +7,9 @@ module Main where
 data Binary = Bin Binary Binary | Tip
   deriving Show
 
+encode :: Binary -> [Bool]
+encode (Bin l r) = True : (encode l) ++ (encode r)
+encode Tip = [False]
+
 main :: IO ()
 main = return ()
