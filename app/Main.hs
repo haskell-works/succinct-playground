0e104e300a0ee1694027bd27e5fe871c6627bd3c
John Ky <newhoggy@gmail.com>
2016-01-21 23:36:10 +1100

    Select function.

--- app/Main.hs
+++ app/Main.hs
@@ -12,10 +12,18 @@ encode (Bin l r) = True : (encode l) ++ (encode r)
 encode Tip = [False]
 
 rank :: Eq a => a -> [a] -> Int -> Int
-rank _ _      0 = 0
-rank a (x:xs) i | a == x = 1 + (rank a xs (i - 1))
-rank a (_:xs) i = (rank a xs (i - 1))
-rank _ []     _ = 0
+rank _ _      0           = 0
+rank a (x:xs) i | a == x  = 1 + (rank a xs (i - 1))
+rank a (_:xs) i           = 0 + (rank a xs (i - 1))
+rank _ []     _           = 0
+
+select :: Eq a => a -> [a] -> Int -> Int
+select _ _      0          = -1
+select a (x:xs) i | a == x = 1 + (select a xs (i - 1))
+select a (_:xs) i          = 1 + (select a xs (i - 0))
+select _ []     _          = 0
+
+-- Jacobson encoding
 
 main :: IO ()
 main = return ()
